a7e080a56e42b93c1d47a08665da85e2f840136d
WizardOzzy <121866492+WizardOzzy@users.noreply.github.com>
2023-03-30 15:50:05 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+
+valid_email = "hapada4412@invodua.com"
+valid_password = "12345qwerty"
+invalid_password = "qwerty15423"
+invalid_email = "hapada432@invodua.com"
+invalid_key = {"key": "12412431241351354124315412412"}
